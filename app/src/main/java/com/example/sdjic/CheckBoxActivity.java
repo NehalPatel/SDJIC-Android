bf3e379d3af03a25646c0357bdfc287b09cde4f4
Nehal <patelnehal4u@gmail.com>
2021-10-04 23:05:36 +0530

    Toggle button and checkbox demo fixed

--- app/src/main/java/com/example/sdjic/CheckBoxActivity.java
+++ app/src/main/java/com/example/sdjic/CheckBoxActivity.java
@@ -3,12 +3,45 @@ package com.example.sdjic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class CheckBoxActivity extends AppCompatActivity {
+
+    CheckBox tea, coffee, pizza;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_box);
+
+        tea = findViewById(R.id.checkBoxTea);
+        coffee = findViewById(R.id.checkBoxCoffee);
+        pizza = findViewById(R.id.checkBoxPizza);
+    }
+
+    public void onPlaceOrderClick(View view) {
+        int total = 0;
+        StringBuilder result = new StringBuilder();
+
+        if(tea.isChecked()){
+            total += 10;
+            result.append(tea.getText() + "\n");
+        }
+
+        if(coffee.isChecked()){
+            total += 20;
+            result.append(coffee.getText() + "\n");
+        }
+
+        if(pizza.isChecked()){
+            total += 100;
+            result.append(pizza.getText() + "\n");
+        }
+
+        result.append("\n Total:" + total);
+
+        Toast.makeText(getApplicationContext(), result.toString(), Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/sdjic/ToggleButtonActivity.java
+++ app/src/main/java/com/example/sdjic/ToggleButtonActivity.java
@@ -9,24 +9,41 @@ import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class ToggleButtonActivity extends AppCompatActivity {
+    ToggleButton t1, t2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toggle_button);
+
+        t1 = findViewById(R.id.toggleButtonSendSMS);
+        t2 = findViewById(R.id.toggleButtonSendNotification);
     }
 
     public void onSaveSettingsClick(View view){
         StringBuilder result = new StringBuilder();
 
-        ToggleButton t1 = findViewById(R.id.toggleButtonSendSMS);
-        ToggleButton t2 = findViewById(R.id.toggleButtonSendNotification);
-
         result.append("Send SMS: ").append( t1.getText() + "\n");
         result.append("Send Notification: ").append( t2.getText() + "\n");
 
-        Log.d("testing", result.toString());
+        Toast.makeText(getApplicationContext(), result.toString(), Toast.LENGTH_LONG).show();
+    }
 
-        Toast.makeText(getApplicationContext(), "Hello world", Toast.LENGTH_LONG).show();
+    public void onGetOffText(View view){
+        StringBuilder result = new StringBuilder();
+
+        result.append("Send SMS (OFF): ").append( t1.getTextOff() + "\n");
+        result.append("Send Notification (OFF): ").append( t2.getTextOff() + "\n");
+
+        Toast.makeText(getApplicationContext(), result.toString(), Toast.LENGTH_LONG).show();
+    }
+
+    public void onGetOnText(View view){
+        StringBuilder result = new StringBuilder();
+
+        result.append("Send SMS (ON): ").append( t1.getTextOn() + "\n");
+        result.append("Send Notification (ON): ").append( t2.getTextOn() + "\n");
+
+        Toast.makeText(getApplicationContext(), result.toString(), Toast.LENGTH_LONG).show();
     }
 }
