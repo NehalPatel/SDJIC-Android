f280957792d87ac0275829805934471b142d4dd9
Nehal <patelnehal4u@gmail.com>
2022-01-11 00:42:24 +0530

    Date picker demo added.

--- app/src/main/java/com/example/sdjic/DateActivity.java
+++ app/src/main/java/com/example/sdjic/DateActivity.java
@@ -1,17 +1,25 @@
 package com.example.sdjic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class DateActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
 
-    TextView txtDate;
+public class DateActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    TextView txtDate, txtBirthday;
     DatePicker dpDate;
+    ImageButton dtImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +37,16 @@ public class DateActivity extends AppCompatActivity {
                 }
             });
         }
+
+        txtBirthday = findViewById(R.id.txtBirthday);
+        dtImage = findViewById(R.id.btnDTimage);
+        dtImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "Date Picker");
+            }
+        });
     }
 
     public void onUserDateSelect(View view)
@@ -39,5 +57,15 @@ public class DateActivity extends AppCompatActivity {
         builder.append(dpDate.getYear());
 
         txtDate.setText("Current Date:" + builder.toString());
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, day);
+        String currentDateString = DateFormat.getDateInstance(DateFormat.MEDIUM).format(c.getTime());
+        txtBirthday.setText(currentDateString);
     }
 }
