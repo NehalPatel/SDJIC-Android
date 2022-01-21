a413fcd01900dafe3b37895b4cb26b2caabfe5e0
Nehal Patel <patelnehal4u@gmail.com>
2022-01-21 02:34:00 -0800

    Horizontal/Vertical scroll view demo added.

---
+++ app/src/main/java/com/example/sdjic/VerticalActivity.java
@@ -0,0 +1,14 @@
+package com.example.sdjic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VerticalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vertical);
+    }
+}
